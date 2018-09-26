2df14ff8a0098cbdd533182fc1f6a530354768c5
Endless <loli.con@qq.com>
2018-09-26 13:07:32 +0800

    剪枝：检测浮牌（提升 7-10 倍的性能）

--- algo_test.go
+++ algo_test.go
@@ -5,7 +5,10 @@ import (
 )
 
 func TestCheckWin(t *testing.T) {
-	_, cnt := convert("111234678m dong dong dong xi xi")
+	_, cnt, err := convert("111234678m dong dong dong xi xi")
+	if err != nil {
+		t.Error(err)
+	}
 	if !checkWin(cnt) {
 		t.Error("checkWin")
 	}
