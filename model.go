bc9fc880a9b27e65163b09a9007e3c0536df198b
Endless <loli.con@qq.com>
2018-10-01 17:50:18 +0800

    输出全部是中文

--- model.go
+++ model.go
@@ -28,6 +28,26 @@ func (nt needTiles) parse() (allCount int, tiles []string) {
 	return allCount, tiles
 }
 
+func (nt needTiles) parseZH() (allCount int, tilesZH []string) {
+	if len(nt) == 0 {
+		return 0, nil
+	}
+
+	tileIndexes := make([]int, 0, len(nt))
+	for idx, cnt := range nt {
+		tileIndexes = append(tileIndexes, idx)
+		allCount += cnt
+	}
+	sort.Ints(tileIndexes)
+
+	tilesZH = make([]string, len(tileIndexes))
+	for i, idx := range tileIndexes {
+		tilesZH[i] = mahjongZH[idx]
+	}
+
+	return allCount, tilesZH
+}
+
 func (nt needTiles) tiles() []string {
 	tiles := make([]string, 0, len(nt))
 	for k := range nt {
