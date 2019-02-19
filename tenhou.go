c1dd86f9f1d89763e19417f6e332fb0f8ad0103e
Endless <loli.con@qq.com>
2019-02-19 11:52:15 +0800

    完善天凤消息解析

--- tenhou.go
+++ tenhou.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"regexp"
 	"strings"
 	"strconv"
 	"fmt"
@@ -20,7 +19,7 @@ type tenhouMessage struct {
 	//J string `json:"j"`
 	//G string `json:"g"`
 
-	// 新的一局 tag=INIT
+	// round 开始 tag=INIT
 	Seed string `json:"seed"` // 本局信息：场数，连庄棒数，立直棒数，骰子A减一，骰子B减一，宝牌指示牌 1,0,0,3,2,92
 	Ten  string `json:"ten"`  // 各家点数 280,230,240,250
 	Oya  string `json:"oya"`  // 庄家 2
@@ -43,19 +42,19 @@ type tenhouMessage struct {
 	// `json:"ten"` // 立直成功后的各家点数 250,250,240,250
 	// `json:"step"` // 2
 
-	// 自摸/有人放铳 tag=牌
-	T string `json:"t"` // 选项：8为和牌
+	// 自摸/有人放铳 tag=牌, t>=8
+	T string `json:"t"` // 选项
 
 	// 和牌 tag=AGARI
 	// ba, hai, m, machi, ten, yaku, doraHai, who, fromWho, sc
 	//Ba string `json:"ba"` // 0,0
 	// `json:"hai"` // 和牌型 8,9,11,14,19,125,126,127
 	// `json:"m"` // 副露编号 13527,50794
-	//Machi string `json:"machi"` // 自摸/荣和的牌 126
+	//Machi string `json:"machi"` // (待ち) 自摸/荣和的牌 126
 	// `json:"ten"` // 符数和点数 30,7700,0
-	//Yaku       string `json:"yaku"`       // 翻数详情（编号，翻数） 18,1,20,1,34,2
-	//DoraHai    string `json:"doraHai"`    // 宝牌 123
-	//DoraHaiUra string `json:"doraHaiUra"` // 里宝牌 77
+	//Yaku        string `json:"yaku"`       // 役（编号，翻数） 18,1,20,1,34,2
+	//DoraTile    string `json:"doraHai"`    // 宝牌 123
+	//UraDoraTile string `json:"doraHaiUra"` // 里宝牌 77
 	// `json:"who"` // 和牌者 1
 	//FromWho string `json:"fromWho"` // 点炮者（自摸时为和牌者） 0
 	//Score   string `json:"sc"`      // 各家增减分 260,-77,310,77,220,0,210,0
@@ -82,8 +81,6 @@ type tenhouMessage struct {
 	//Kawa3 string `json:"kawa3"`
 }
 
-var tileReg = regexp.MustCompile(``)
-
 // 0-35 m
 // 36-71 p
 // 72-107 s
@@ -100,7 +97,8 @@ func (m *tenhouMessage) analysis(counts []int) error {
 	fmt.Println("收到", m.Tag)
 
 	switch m.Tag {
-	case "INIT":
+	case "INIT", "REINIT":
+		// round 开始/重连
 		for i := range counts {
 			counts[i] = 0
 		}
@@ -108,27 +106,54 @@ func (m *tenhouMessage) analysis(counts []int) error {
 			index := m._parseTenhouTile(pai)
 			counts[index]++
 		}
-		if err := _analysis(13, counts); err != nil {
-			return err
+		return _analysis(13, counts)
+	case "N":
+		// 某人副露
+		if m.Who == "0" {
+
 		}
+	case "DORA":
+		// 杠宝牌
+		// 1. 能摸的牌减少
+		// 2. 打点提高
+	case "REACH":
+		// 如果是他家立直，进入攻守判断模式
+		if m.Step != "1" {
+			break
+		}
+	case "AGARI", "RYUUKYOKU":
+		// round 结束
+	case "PROF":
+		// 游戏结束
+	case "BYE":
+		// 某人退出
+	case "REJOIN", "GO":
+		// 重连
+	case "FURITEN":
+		// 振听
+	case "TAIKYOKU", "UN", "LN":
+		// 其他
 	default:
-		if m.Tag[0] == 'T' {
+		switch m.Tag[0] {
+		case 'T':
+			// 摸牌
 			index := m._parseTenhouTile(m.Tag[1:])
 			counts[index]++
-			if err := _analysis(14, counts); err != nil {
-				return err
-			}
-		} else if m.Tag[0] == 'D' {
+			return _analysis(14, counts)
+		case 'D':
+			// 自家舍牌
 			index := m._parseTenhouTile(m.Tag[1:])
 			counts[index]--
-			if err := _analysis(13, counts); err != nil {
-				return err
+			return _analysis(13, counts)
+		case 'e', 'f', 'g', 'E', 'F', 'G':
+			// 他家舍牌
+			//index := m._parseTenhouTile(m.Tag[1:])
+			//isTsumogiri := m.Tag[0] >= 'a' // 自模切
+			// TODO: 添加到舍牌列表中
+			if m.T != "" {
+				// 是否副露，何切
 			}
-		} else if tileReg.MatchString(m.Tag) {
-			// 舍牌
-			//fmt.Println(m.Tag)
-		} else {
-			//fmt.Println(m.Tag)
+		default:
 		}
 	}
 
