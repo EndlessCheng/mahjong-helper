d96b3158b5d7c85f089715e9b4e074e29c4279a1
Endless <loli.con@qq.com>
2019-03-03 16:12:55 +0800

    向听倒退没有优势的情况下不需要打印向听倒退的内容

---
+++ util/agari_rate.go
@@ -0,0 +1,41 @@
+package util
+
+var (
+	// 仅为无筋数据，未考虑筋牌、早外、NC、是否为宝牌、其他场况等，仅供参考
+	// 6~10巡目 [牌0-4][剩余数1-4]
+	// from: 勝つための現代麻雀技術論
+	agariTable = [...][4]float64{
+		{26.3, 41.6, 50.1, 55.0},
+		{19.2, 31.7, 38.2, 42.0},
+		{14.8, 25.5, 32.0, 36.8},
+		{11.8, 20.3, 26.7, 31.0},
+		{11.8, 20.3, 26.7, 31.0},
+	}
+
+	// 8巡目 [剩余数1-3]
+	// from:「統計学」のマージャン戦術
+	// FIXME: 这条仅适用于单骑，双碰不适用
+	agariZiTable = [3]float64{47.5, 58.0, 49.5}
+)
+
+// selfDiscards: 自家舍牌，用于分析骗筋时的和率
+func CalculateAgariRate(waits Waits, selfDiscards []int) float64 {
+	agariRate := 0.0
+	for tile, left := range waits {
+		if left == 0 {
+			continue
+		}
+		var rate float64
+		if tile < 27 {
+			tile %= 9
+			if tile > 4 {
+				tile = 8 - tile
+			}
+			rate = agariTable[tile][left-1]
+		} else {
+			rate = agariZiTable[left-1]
+		}
+		agariRate = agariRate + rate - agariRate*rate/100
+	}
+	return agariRate
+}
