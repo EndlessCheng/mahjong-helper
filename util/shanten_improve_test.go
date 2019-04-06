bfd13349cbde63af9f19e15292ab79045d7d5d11
Endless <loli.con@qq.com>
2019-04-06 11:07:30 +0800

    进张相差在 10% 以内的，AvgNextShantenWaitsCount 多者为优，但是若 AvgNextShantenWaitsCount 差距太小（10%）还是看进张

--- util/shanten_improve_test.go
+++ util/shanten_improve_test.go
@@ -120,17 +120,19 @@ func TestCalculateShantenWithImproves14Open(t *testing.T) {
 
 func TestCalculateMeld(t *testing.T) {
 	tiles := "1234m 112z"
-	t.Log("原手牌")
+	tiles = "23445667m 99p"
 	result := CalculateShantenWithImproves13(MustStrToTiles34(tiles), true)
+	t.Log("原手牌" + NumberToChineseShanten(result.Shanten))
 	t.Log(result)
 
 	tile := "1m"
+	tile = "3m" // "9p"
 	shanten, results, incShantenResults := CalculateMeld(MustStrToTiles34(tiles), MustStrToTile34(tile), true)
-	t.Log("鸣牌" + NumberToChineseShanten(shanten))
+	t.Log("鸣牌后" + NumberToChineseShanten(shanten))
 	for _, result := range results {
 		t.Log(result)
 	}
-	t.Log("鸣牌" + NumberToChineseShanten(shanten + 1))
+	t.Log("鸣牌后" + NumberToChineseShanten(shanten+1))
 	for _, result := range incShantenResults {
 		t.Log(result)
 	}
