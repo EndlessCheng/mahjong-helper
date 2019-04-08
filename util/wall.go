105558587c8427ff09fc5efb8fe7e49bc5c6b0b2
Endless <loli.con@qq.com>
2019-04-09 00:13:52 +0800

    添加 CalcDNCSafeTiles，用来计算只输单骑、双碰的牌

--- util/wall.go
+++ util/wall.go
@@ -55,75 +55,159 @@ func (l WallSafeTileList) FilterWithHands(handsTiles34 []int) WallSafeTileList {
 	return newSafeTiles34
 }
 
-// 根据剩余牌 leftTiles34 中的某些牌是否为 0，来判断哪些牌较为安全（只输双碰、单骑、边张、坎张）
-func CalcNCSafeTiles34(leftTiles34 []int) (ncSafeTiles34 WallSafeTileList) {
+// 根据剩余牌 leftTiles34 中的某些牌是否为 0，来判断哪些牌非常安全（Double No Chance：只输单骑、双碰）
+func CalcDNCSafeTiles(leftTiles34 []int) (dncSafeTiles []int) {
 	const leftLimit = 0
+	nc := func(idx int) bool {
+		return leftTiles34[idx] == leftLimit
+	}
+	or := func(idx ...int) bool {
+		for _, i := range idx {
+			if nc(i) {
+				return true
+			}
+		}
+		return false
+	}
+	and := func(idx ...int) bool {
+		for _, i := range idx {
+			if !nc(i) {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := 0; i < 3; i++ {
+		// 2/3断的1
+		if or(9*i+1, 9*i+2) {
+			dncSafeTiles = append(dncSafeTiles, 9*i)
+		}
+		// 3/14断的2
+		if nc(9*i+2) || and(9*i, 9*i+3) {
+			dncSafeTiles = append(dncSafeTiles, 9*i+1)
+		}
+		// 14/24/25断的3（4567同理）
+		for j := 2; j <= 6; j++ {
+			idx := 9*i + j
+			if and(idx-2, idx+1) || and(idx-1, idx+1) || and(idx-1, idx+2) {
+				dncSafeTiles = append(dncSafeTiles, idx)
+			}
+		}
+		// 7/69断的8
+		if nc(9*i+6) || and(9*i+5, 9*i+8) {
+			dncSafeTiles = append(dncSafeTiles, 9*i+7)
+		}
+		// 7/8断的9
+		if or(9*i+6, 9*i+7) {
+			dncSafeTiles = append(dncSafeTiles, 9*i+8)
+		}
+	}
+	return
+}
+
+// 根据剩余牌 leftTiles34 中的某些牌是否为 0，来判断哪些牌较为安全（No Chance：只输单骑、双碰、边张、坎张）
+func CalcNCSafeTiles(leftTiles34 []int) (ncSafeTiles WallSafeTileList) {
+	const leftLimit = 0
+	nc := func(idx int) bool {
+		return leftTiles34[idx] == leftLimit
+	}
+	or := func(idx ...int) bool {
+		for _, i := range idx {
+			if nc(i) {
+				return true
+			}
+		}
+		return false
+	}
+
 	const safeType = WallSafeTypeNC
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			idx := 9*i + j
-			if leftTiles34[idx+1] == leftLimit || leftTiles34[idx+2] == leftLimit {
-				ncSafeTiles34 = append(ncSafeTiles34, WallSafeTile{idx, safeType})
+			if or(idx+1, idx+2) {
+				ncSafeTiles = append(ncSafeTiles, WallSafeTile{idx, safeType})
 			}
 		}
 		for j := 3; j < 6; j++ {
 			idx := 9*i + j
-			if (leftTiles34[idx-2] == leftLimit || leftTiles34[idx-1] == leftLimit) && (leftTiles34[idx+1] == leftLimit || leftTiles34[idx+2] == leftLimit) {
-				ncSafeTiles34 = append(ncSafeTiles34, WallSafeTile{idx, safeType})
+			if or(idx-2, idx-1) && or(idx+1, idx+2) {
+				ncSafeTiles = append(ncSafeTiles, WallSafeTile{idx, safeType})
 			}
 		}
 		for j := 6; j < 9; j++ {
 			idx := 9*i + j
-			if leftTiles34[idx-2] == leftLimit || leftTiles34[idx-1] == leftLimit {
-				ncSafeTiles34 = append(ncSafeTiles34, WallSafeTile{idx, safeType})
+			if or(idx-2, idx-1) {
+				ncSafeTiles = append(ncSafeTiles, WallSafeTile{idx, safeType})
 			}
 		}
 	}
-	ncSafeTiles34.sort()
+	ncSafeTiles.sort()
 	return
 }
 
-func CalcOCSafeTiles34(leftTiles34 []int) (ocSafeTiles34 WallSafeTileList) {
+// 根据剩余牌 leftTiles34 中的某些牌是否为 1，来判断哪些牌较为安全（One Chance：早巡大概率只输单骑、双碰、边张、坎张）
+func CalcOCSafeTiles(leftTiles34 []int) (ocSafeTiles WallSafeTileList) {
 	const leftLimit = 1
+	nc := func(idx int) bool {
+		return leftTiles34[idx] == leftLimit
+	}
+	or := func(idx ...int) bool {
+		for _, i := range idx {
+			if nc(i) {
+				return true
+			}
+		}
+		return false
+	}
+	and := func(idx ...int) bool {
+		for _, i := range idx {
+			if !nc(i) {
+				return false
+			}
+		}
+		return true
+	}
+
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			idx := 9*i + j
-			if leftTiles34[idx+1] == leftLimit && leftTiles34[idx+2] == leftLimit {
-				ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Double})
-			} else if leftTiles34[idx+1] == leftLimit || leftTiles34[idx+2] == leftLimit {
-				ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Single})
+			if and(idx+1, idx+2) {
+				ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Double})
+			} else if or(idx+1, idx+2) {
+				ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Single})
 			}
 		}
 		for j := 3; j < 6; j++ {
 			idx := 9*i + j
-			if (leftTiles34[idx-2] == leftLimit || leftTiles34[idx-1] == leftLimit) && (leftTiles34[idx+1] == leftLimit || leftTiles34[idx+2] == leftLimit) {
-				if leftTiles34[idx-2] == leftLimit && leftTiles34[idx-1] == leftLimit && leftTiles34[idx+1] == leftLimit && leftTiles34[idx+2] == leftLimit {
+			if or(idx-2, idx-1) && or(idx+1, idx+2) {
+				if and(idx-2, idx-1, idx+1, idx+2) {
 					// 两边都是 double
-					ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Double})
-				} else if leftTiles34[idx-2] == leftLimit && leftTiles34[idx-1] == leftLimit || leftTiles34[idx+1] == leftLimit && leftTiles34[idx+2] == leftLimit {
+					ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Double})
+				} else if and(idx-2, idx-1) || and(idx+1, idx+2) {
 					// 一半 double，一半不是 double
-					ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Mix})
+					ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Mix})
 				} else {
-					ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Single})
+					ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Single})
 				}
 			}
 		}
 		for j := 6; j < 9; j++ {
 			idx := 9*i + j
-			if leftTiles34[idx-2] == leftLimit && leftTiles34[idx-1] == leftLimit {
-				ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Double})
-			} else if leftTiles34[idx-2] == leftLimit || leftTiles34[idx-1] == leftLimit {
-				ocSafeTiles34 = append(ocSafeTiles34, WallSafeTile{idx, WallSafeTypeOC_Single})
+			if and(idx-2, idx-1) {
+				ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Double})
+			} else if or(idx-2, idx-1) {
+				ocSafeTiles = append(ocSafeTiles, WallSafeTile{idx, WallSafeTypeOC_Single})
 			}
 		}
 	}
-	ocSafeTiles34.sort()
+	ocSafeTiles.sort()
 	return
 }
 
-func CalcWallTiles34(leftTiles34 []int) (safeTiles34 WallSafeTileList) {
-	safeTiles34 = append(safeTiles34, CalcNCSafeTiles34(leftTiles34)...)
-	safeTiles34 = append(safeTiles34, CalcOCSafeTiles34(leftTiles34)...)
-	safeTiles34.sort()
+func CalcWallTiles(leftTiles34 []int) (safeTiles WallSafeTileList) {
+	safeTiles = append(safeTiles, CalcNCSafeTiles(leftTiles34)...)
+	safeTiles = append(safeTiles, CalcOCSafeTiles(leftTiles34)...)
+	safeTiles.sort()
 	return
 }
