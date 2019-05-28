e6f55dc552d2e1f65b5511c29174cf19666a37f3
Endless <loli.con@qq.com>
2019-05-28 16:59:28 +0800

    自动获取 startLo 和 accountID

--- core_test.go
+++ core_test.go
@@ -21,18 +21,28 @@ func Test_majsoul_analysis(t *testing.T) {
 		Message string `json:"message"`
 	}{}
 
-	//
+	// config
 	accountID := gameConf.MajsoulAccountID
-	startLo := 26867
+	startLo := -1
 	endLo := -1
 
 	majsoulRoundData := &majsoulRoundData{accountID: accountID}
 	majsoulRoundData.roundData = newRoundData(majsoulRoundData, 0, 0)
 
 	lines := strings.Split(string(logData), "\n")
+	if startLo == -1 {
+		// 取最近游戏的日志
+		for i := len(lines) - 1; i >= 0; i-- {
+			if strings.Contains(lines[i], "==============") {
+				startLo = i + 3
+				break
+			}
+		}
+	}
 	if endLo == -1 {
 		endLo = len(lines)
 	}
+
 	for lo, line := range lines[startLo-1 : endLo] {
 		debug.Lo = lo + 1
 		fmt.Println(debug.Lo)
@@ -49,6 +59,12 @@ func Test_majsoul_analysis(t *testing.T) {
 		d := majsoulMessage{}
 		if err := json.Unmarshal([]byte(msg), &d); err != nil {
 			fmt.Println(err)
+			continue
+		}
+
+		if d.AccountID > 0 {
+			majsoulRoundData.accountID = d.AccountID
+			printAccountInfo(d.AccountID)
 			continue
 		}
 
@@ -72,13 +88,28 @@ func Test_tenhou_analysis(t *testing.T) {
 		Message string `json:"message"`
 	}{}
 
-	//
-	startLo := 10274
+	// config
+	startLo := -1
+	endLo := -1
 
 	tenhouRoundData := &tenhouRoundData{isRoundEnd: true}
 	tenhouRoundData.roundData = newRoundData(tenhouRoundData, 0, 0)
 
-	for lo, line := range strings.Split(string(logData), "\n")[startLo-1:] {
+	lines := strings.Split(string(logData), "\n")
+	if startLo == -1 {
+		// 取最近游戏的日志
+		for i := len(lines) - 1; i >= 0; i-- {
+			if strings.Contains(lines[i], "==============") {
+				startLo = i + 3
+				break
+			}
+		}
+	}
+	if endLo == -1 {
+		endLo = len(lines)
+	}
+
+	for lo, line := range lines[startLo-1 : endLo] {
 		debug.Lo = lo + 1
 		fmt.Println(debug.Lo)
 		if line == "" {
