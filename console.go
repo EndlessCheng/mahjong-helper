dcdef3422b8cf246bdb74d95b1673a41066f1f3e
Endless <loli.con@qq.com>
2019-06-22 10:29:57 +0800

    重构

--- console.go
+++ console.go
@@ -6,28 +6,24 @@ import (
 	"runtime"
 )
 
-var clearFuncMap map[string]func() //create a map for storing clear funcs
+var clearFuncMap = map[string]func(){}
 
 func init() {
-	clearFuncMap = make(map[string]func()) //Initialize it
 	clearFuncMap["linux"] = func() {
-		cmd := exec.Command("clear") //Linux example, its tested
+		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
 	clearFuncMap["darwin"] = clearFuncMap["linux"]
 	clearFuncMap["windows"] = func() {
-		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
 }
 
 func clearConsole() {
-	clearFunc, ok := clearFuncMap[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-	if ok { //if we defined a clear func for that platform:
-		clearFunc() //we execute it
-	} else { //unsupported platform
-		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	if clearFunc, ok := clearFuncMap[runtime.GOOS]; ok {
+		clearFunc()
 	}
 }
