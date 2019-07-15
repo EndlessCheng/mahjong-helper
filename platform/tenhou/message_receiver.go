e4537dbb0045aa06ed0766a4193147e964b831c2
Endless <loli.con@qq.com>
2019-07-15 10:44:18 +0800

    简化代码

--- platform/tenhou/message_receiver.go
+++ platform/tenhou/message_receiver.go
@@ -9,7 +9,6 @@ import (
 type MessageReceiver struct {
 	originMessageQueue  chan []byte
 	orderedMessageQueue chan []byte
-	outMessage          chan []byte
 }
 
 func NewMessageReceiver() *MessageReceiver {
@@ -17,9 +16,8 @@ func NewMessageReceiver() *MessageReceiver {
 	mr := &MessageReceiver{
 		originMessageQueue:  make(chan []byte, maxQueueSize),
 		orderedMessageQueue: make(chan []byte, maxQueueSize),
-		outMessage:          make(chan []byte),
 	}
-	mr.run()
+	go mr.run()
 	return mr
 }
 
@@ -37,34 +35,26 @@ func (mr *MessageReceiver) isSelfDraw(data []byte) bool {
 }
 
 func (mr *MessageReceiver) run() {
-	go func() {
-		for data := range mr.originMessageQueue {
-			if !mr.isSelfDraw(data) {
-				mr.orderedMessageQueue <- data
-				continue
-			}
-
-			// 收到了自家摸牌的消息，则等待一段很短的时间
-			time.Sleep(75 * time.Millisecond) // 实际间隔在 3~9ms
-
-			// 未收到新数据
-			if len(mr.originMessageQueue) == 0 {
-				mr.orderedMessageQueue <- data
-				continue
-			}
-
-			// 在短时间内收到了新数据
-			// 因为摸牌后肯定要等待玩家操作，正常情况是不会马上有新数据的，所以这说明前端乱序发来了数据
-			// 把 data 重新塞回去，这样才是正确的顺序
-			mr.originMessageQueue <- data
+	for data := range mr.originMessageQueue {
+		if !mr.isSelfDraw(data) {
+			mr.orderedMessageQueue <- data
+			continue
 		}
-	}()
 
-	go func() {
-		for data := range mr.orderedMessageQueue {
-			mr.outMessage <- data
+		// 收到了自家摸牌的消息，则等待一段很短的时间
+		time.Sleep(75 * time.Millisecond) // 实际间隔在 3~9ms
+
+		// 未收到新数据
+		if len(mr.originMessageQueue) == 0 {
+			mr.orderedMessageQueue <- data
+			continue
 		}
-	}()
+
+		// 在短时间内收到了新数据
+		// 因为摸牌后肯定要等待玩家操作，正常情况是不会马上有新数据的，所以这说明前端乱序发来了数据
+		// 把 data 重新塞回去，这样才是正确的顺序
+		mr.originMessageQueue <- data
+	}
 }
 
 func (mr *MessageReceiver) Put(data []byte) {
@@ -72,10 +62,9 @@ func (mr *MessageReceiver) Put(data []byte) {
 }
 
 func (mr *MessageReceiver) Get() []byte {
-	return <-mr.outMessage
+	return <-mr.orderedMessageQueue
 }
 
-// TODO
-//func (mr *MessageReceiver) IsEmpty() bool {
-//	return
-//}
+func (mr *MessageReceiver) IsEmpty() bool {
+	return len(mr.originMessageQueue) == 0 && len(mr.orderedMessageQueue) == 0
+}
