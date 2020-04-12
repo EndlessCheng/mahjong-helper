07844a66eee01034ddf59a26bb7350c740728ae4
Wang Chengke <wongck@pku.edu.cn>
2020-04-12 14:14:49 +0800

    [webapi] send request periodically (via ajax)

--- webapi/core.js
+++ webapi/core.js
@@ -3,11 +3,11 @@
 function tile_img_url(index) {
 	function label(index) {
 		let offset = [0, 9, 18, 27, 34];
-		let l = ["m", "s", "p", "z"];
+		let l = ["m", "p", "s", "z"];
 
 		for (let i = 0; i < l.length; i++) {
 			if (index < offset[i+1]) {
-				return (index - offset[i]) + l[i];
+				return (index - offset[i] + 1) + l[i];
 			}
 		}
 		throw new Error("invalid index");
@@ -40,80 +40,22 @@ function show_tiles(data) {
 	}
 }
 
-var data = {
-    "counts": [
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        1,
-        0,
-        2,
-        0,
-        0,
-        0,
-        0,
-        0
-    ],
-    "risk": [
-        6.8689162795508745,
-        1.3130278422273782,
-        14.364682863109048,
-        26.086178013431933,
-        0,
-        3.996171693735499,
-        26.609623463447978,
-        10.24728615971008,
-        4.707313155452437,
-        1.62,
-        12.041053092734451,
-        4.955252900232019,
-        4.019039743204979,
-        31.297173259860788,
-        22.897446084686777,
-        13.089059187935034,
-        21.93600580280426,
-        20.047425288863106,
-        2.1125103680057706,
-        4.555635730858469,
-        23.916679946635732,
-        18.880975342227377,
-        22.090670997703363,
-        35.24169806753138,
-        21.933339973317867,
-        4.555635730858469,
-        20.978551860920128,
-        0,
-        1.04143593387471,
-        0,
-        0.8999999999999999,
-        2.519098836845261,
-        0.14272041763341067,
-        17.777878904753784
-    ],
-    "timestamp": 0
+let interval = 1000;
+var backoff = interval;
+
+function update_tile() {
+	$.getJSON("/api")
+	.done(function(data) {
+		show_tiles(data);
+		backoff = interval;
+		setTimeout(update_tile, interval);
+	})
+	.fail(function( jqxhr, textStatus, error ) {
+		var err = textStatus + ", " + error;
+		console.log("Request Failed: " + err);
+		backoff = backoff * 2;
+		setTimeout(update_tile, backoff);
+	});
 }
 
-show_tiles(data)
+setTimeout(update_tile, interval);
